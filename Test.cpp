c2f9e581d9b9e2e855fadb979cecae8b1aca601c
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-03-07 10:55:48 +0200

    update in snowman.cpp and Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,20 +1,40 @@
+#include <iostream>
+
 #include "doctest.h"
 #include "snowman.hpp"
+
 using namespace ariel;
 
 #include <string>
 using namespace std;
 
+TEST_CASE("Test length-7") {
+    CHECK_THROWS(snowman(1114441));
+}
+
+TEST_CASE("Test length-9") {
+    CHECK_THROWS(snowman(111444221));
+}
+
+TEST_CASE("Test length-3 and invalid input range") {
+    CHECK_THROWS(snowman(555));
+}
+
+TEST_CASE("Test invalid input range-1") {
+    CHECK_THROWS(snowman(11140221));
+}
+
+TEST_CASE("Test invalid input range-2") {
+    CHECK_THROWS(snowman(11141251));
+}
 
 TEST_CASE("Good snowman code") {
+    cout << string(snowman(11114411));
+    cout << string("_===_\n(.,.)\n( : )\n( : )");
     CHECK(snowman(11114411) == string("_===_\n(.,.)\n( : )\n( : )"));
-    /* Add more checks here */
 }
 
-TEST_CASE("Bad snowman code") {
-    CHECK_THROWS(snowman(555));
-    /* Add more checks here */
-}
+
 
 
 /* Add more test cases here */
--- snowman.cpp
+++ snowman.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <string>
-
+#include <stdexcept>
 #include "snowman.hpp"
 
 using namespace std;
 
+const unsigned int MAX_NUMS = 8;
+const std::string SPACE = "?";
+
 namespace ariel {
 
+    // hat
     std::string H(char c) {
         switch(c) {
         case '1':
-            return "     \n_===_\n";
+            return "_===_\n";
         case '2':
             return " ___ \n.....\n";
         case '3':
@@ -22,6 +26,7 @@ namespace ariel {
         }
     }
 
+    // nose/mouth
     std::string N(char c) {
         switch(c) {
         case '1':
@@ -35,6 +40,7 @@ namespace ariel {
         }
     }
 
+    // left/right eye
     std::string LR(char c) {
         switch(c) {
         case '1':
@@ -50,6 +56,7 @@ namespace ariel {
         }
     }
 
+    // left arm
     std::string X(char c) {
         switch(c) {
         case '1':
@@ -63,6 +70,7 @@ namespace ariel {
         }
     }
 
+    // right arm
     std::string Y(char c) {
         switch(c) {
         case '1':
@@ -76,6 +84,7 @@ namespace ariel {
         }
     }
 
+    // torso
     std::string T(char c) {
         switch(c) {
         case '1':
@@ -89,6 +98,7 @@ namespace ariel {
         }
     }
 
+    // base
     std::string B(char c) {
         switch(c) {
         case '1':
@@ -104,41 +114,55 @@ namespace ariel {
 
     std::string snowman(unsigned int id) {
 
-        std::string seq = std::to_string(id);
-        std::string result = "";
+        // casting the id to a string and create result string
+        std::string seq = std::to_string(id), result = "";
+
+        unsigned int temp = id, i = 0;
+        
+        while (temp != 0) {
+            // checking if the id contains only 1-4 digits
+            if(temp % 10 < 1 || temp % 10 > 4) {
+                throw std::invalid_argument("Invalid code '" + std::to_string(temp) + "'");
+            }
+            temp = temp / 10;
+            i++;
+        }
 
         // checking if length of id is 8
-        if(seq.length() != 8) {
-            // Exception
-            return "";
-        }
-
-        // checking if the id contains only 1-4 digits
-        for(int i = 0; i < 8; i++) {
-            if((int)(seq[i]) < 1 || (int)(seq[i]) > 4) {
-                // Exception
-                //return "";
-            }
-        }
+        if(i != MAX_NUMS) throw std::invalid_argument("Invalid code '5'");
 
 
         // HNLRXYTB
 
+        
+
+        // hat
         result.append(H(seq[0]));
 
-        result.append(X(seq[4]) + "(");
-        result.append(LR(seq[2]));
-        result.append(N(seq[1]));
-        result.append(LR(seq[3]));
-        result.append(")" + Y(seq[5]) + "\n");
+        // head
+        if(seq[4] == '2') result.append(X(seq[4]));
+        else if(seq[4] != '4') result.append(SPACE);
 
-        result.append(X(seq[4]) + "(");
-        result.append(T(seq[6]));
-        result.append(")" + Y(seq[5]) + "\n");
+        result.append("(" + LR(seq[2]) + N(seq[1]) + LR(seq[3]) + ")");
 
+        if(seq[5] == '2') result.append(Y(seq[5]));
+        result.append("\n");
+
+        // middle
+        if(seq[4] != '2') result.append(X(seq[4]));
+        else if(seq[4] != '4') result.append(SPACE);
+
+        result.append("(" + T(seq[6]) + ")");
+
+        if(seq[5] != '2') result.append(Y(seq[5]));
+        
+        result.append("\n");
+
+        // bottom
+        if(seq[4] != '4') result.append(SPACE);
         result.append("(" + B(seq[7]) + ")");
 
-        
+
         return result;
     }
 };
